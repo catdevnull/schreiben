063f3c20bbdb95c9fb310e49cb4a2841316a99e0
Nulo <git@nulo.in>
2023-03-06 00:05:04 +0000

    activar sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     svelte(),
     svg({
