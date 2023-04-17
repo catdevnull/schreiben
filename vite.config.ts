4ff83ad2a0cc76307d5d700a61582662af212fa6
Nulo <git@nulo.in>
2023-04-17 20:05:40 -0300

    borrar cosas innecesarias y actualizar

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import svg from "@poppanator/sveltekit-svg";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,6 +25,5 @@ export default defineConfig({
     }),
     // lo necesitamos para crypto.subtle para nanoid
     basicSsl(),
-    visualizer(),
   ],
 });
