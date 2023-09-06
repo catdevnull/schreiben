230c6a873252d306fe82b033241d93282ed61df0
Nulo <git@nulo.in>
2023-09-06 18:45:42 -0300

    agregar información del proyecto en ChooseWorld

--- src/views/ChooseWorld.svelte
+++ src/views/ChooseWorld.svelte
@@ -6,8 +6,8 @@
   const worldsPromise = loadWorlds();
 </script>
 
-<header class="my-4 mx-8">
-  <h1 class="font-bold text-3xl">Buen día.</h1>
+<header class="mx-8 my-4">
+  <h1 class="text-3xl font-bold">Buen día.</h1>
   <h3>Elegí un mundo.</h3>
 </header>
 
@@ -23,9 +23,36 @@
     {/each}
     <li>
       <a
-        class="px-6 py-4 bg-slate-200 dark:bg-neutral-800 w-full block rounded-2xl dark:border-neutral-700/70 border my-4 shadow text-xl"
+        class="my-4 block w-full rounded-2xl border bg-slate-200 px-6 py-4 text-xl shadow dark:border-neutral-700/70 dark:bg-neutral-800"
         href={routes.CreateWorld}>Crear mundo</a
       >
     </li>
   </ul>
 {/await}
+
+<div class="prose prose-neutral mx-auto my-8 px-4 dark:prose-invert">
+  <h2>Schreiben es una aplicación para escribir.</h2>
+  <p>
+    Es un experimento para probar tecnologías de CRDT (actualmente <a
+      href="https://github.com/yjs/yjs"
+      rel="noreferrer noopener">Yjs</a
+    >
+    aunque también podría ser
+    <a href="https://automerge.org/" rel="noreferrer noopener">Automerge</a>) e
+    intentar crear con ellas un <em>knowledge base</em>.
+  </p>
+  <p>
+    Schreiben está en desarrollo constante, y no recomiendo que almacenes nada
+    de importancia acá.
+  </p>
+  <p>
+    Schreiben está desarrollado por <a
+      href="https://nulo.ar"
+      rel="noreferrer noopener">Nulo</a
+    >
+    en su tiempo libre, y podés encontrar el código
+    <a href="https://gitea.nulo.in/Nulo/schreiben" rel="noreferrer noopener"
+      >en mi Gitea</a
+    > :)
+  </p>
+</div>
